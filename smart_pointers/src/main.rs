60a403a481d50d910c2efb38309f1e57057f012e
HnryC <maddy@maddy.localdonain>
2021-05-09 17:33:30 +1000

    work

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,7 +1,10 @@
+use crate::List::{Cons, Nil};
+
 enum List {
-    list(i32, list),
-    null
+    Cons(i32, Box<List>),
+    Nil,
 }
 
 fn main() {
+    let list = Cons(0, Box::new(Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Cons(5, Box::new(Nil))))))))))));
 }
